d5b81ceb94518ff98609b4015be343434c2e0cac
Jihiko4450 <anna.demyanenko.4459@mail.ru>
2024-02-13 20:29:58 +0500

    session1

--- app/src/main/java/com/example/rediexpress/MainActivity.kt
+++ app/src/main/java/com/example/rediexpress/MainActivity.kt
@@ -1,24 +1,45 @@
 package com.example.rediexpress
 
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.view.WindowManager
+import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
+import kotlin.math.floor
+import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
+    var secondLeft=0
+    var preff: SharedPreferences?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var main_window:ConstraintLayout=findViewById(R.id.idMain)
+        var logo:ImageView=findViewById(R.id.imageView)
+        preff= getSharedPreferences(TABLE,MODE_PRIVATE)
+        var skip=preff?.getBoolean(key1, false)
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
-        val timer=object :CountDownTimer(3000,1000){
+        val timer=object :CountDownTimer(5000,1000){
             override fun onTick(p0: Long) {
-
+                secondLeft = floor((p0.toFloat() / 1000.0)).roundToInt()
+                if (secondLeft==2) {
+                    main_window.setBackgroundColor(R.color.primarycolor)
+                    logo.setImageResource(R.drawable.logo2)
+                }
             }
 
             override fun onFinish() {
-                var intent= Intent(this@MainActivity, OnboardActivity::class.java)
-                startActivity(intent)
+
+                if (skip==true){
+                    var intent= Intent(this@MainActivity, Holder::class.java)
+                    startActivity(intent)
+                }else{
+                    var intent= Intent(this@MainActivity, OnboardActivity::class.java)
+                    startActivity(intent)
+                }
             }
         }
         timer.start()
--- app/src/main/java/com/example/rediexpress/Onboarding3Fragment.kt
+++ app/src/main/java/com/example/rediexpress/Onboarding3Fragment.kt
@@ -1,6 +1,8 @@
 package com.example.rediexpress
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,6 +12,7 @@ import androidx.appcompat.widget.AppCompatButton
 
 
 class Onboarding3Fragment : Fragment() {
+    var preff: SharedPreferences?=null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,9 +20,11 @@ class Onboarding3Fragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         var q=inflater.inflate(R.layout.fragment_onboarding3, container, false)
+        preff= activity?.getSharedPreferences(TABLE, Context.MODE_PRIVATE)
         var but_sign_up: AppCompatButton =q.findViewById(R.id.buttonsignup)
         but_sign_up.setOnClickListener{
             var intent= Intent(activity, Holder::class.java)
+            preff?.edit()?.putBoolean(key1, true)?.apply()
             startActivity(intent)
         }
         return q    }
