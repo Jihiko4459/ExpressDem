d42b9d023579572b95a3241f2224f057d9093178
Jihiko4450 <anna.demyanenko.4459@mail.ru>
2024-02-26 22:31:03 +0500

    session2верстка

--- app/src/main/java/com/example/rediexpress/ForgotPasswordActivity.kt
+++ app/src/main/java/com/example/rediexpress/ForgotPasswordActivity.kt
@@ -1,11 +1,18 @@
 package com.example.rediexpress
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
 
 class ForgotPasswordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgot_password)
+        var btn_send:AppCompatButton=findViewById(R.id.buttonSend)
+        btn_send.setOnClickListener {
+            var intent= Intent(this@ForgotPasswordActivity, OTPVerifucationActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/rediexpress/LogInActivity.kt
+++ app/src/main/java/com/example/rediexpress/LogInActivity.kt
@@ -1,11 +1,19 @@
 package com.example.rediexpress
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatButton
 
 class LogInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
+        var text_forgot:TextView=findViewById(R.id.textForgotPass)
+        text_forgot.setOnClickListener {
+            val intent=Intent(this@LogInActivity, ForgotPasswordActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
